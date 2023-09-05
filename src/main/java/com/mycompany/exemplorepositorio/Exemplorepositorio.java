82483ce9cbbed60600888070d40ba4c0ecc4e522
KAWAN HERBERT FERREIRA RAMOS <kawan.0946@aluno.pr.senac.br>
2023-09-05 15:48:18 -0300

    primeiro commit'

---
+++ src/main/java/com/mycompany/exemplorepositorio/Exemplorepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplorepositorio;
+
+/**
+ *
+ * @author kawan.0946
+ */
+public class Exemplorepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
